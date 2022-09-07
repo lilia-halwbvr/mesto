b48c3484f5d2c10f7b49ff457bb2e1024d539214
Lilia <lilia@MacBook-Pro-Lilia.local>
2022-09-07 23:25:29 +0400

    add function add cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,109 @@
-let popup = document.querySelector('.popup');
-let popupClose = document.querySelector('.popup__close-button');
+let popupEditProfile = document.querySelector('#popup-edit-profile');
+let popupAddCard = document.querySelector('#popup-add-card')
 let popupName = document.querySelector('#userName-input')
-
-// popupName.value = document.querySelector('.profile__name').textContent 
 let popupJob = document.querySelector('#userAbout-input');
-// popupJob.value = document.querySelector('.profile__about').textContent;
-// let popupSubmit = document.querySelector('.popup__submit-button');
-
 let popupEdit = document.querySelector('.profile__edit-button');
+let popupAdd = document.querySelector('.profile__add-button')
 let popupTitle = document.querySelector('.profile__name');
 let popupSubtitle = document.querySelector('.profile__about');
-let popupForm = document.querySelector('.popup__form');
-// likes = document.querySelectorAll('.element__like-button');
+let popupFormEdit = document.querySelector('.popup__form-edit');
+let closeButtons = document.querySelectorAll('.popup__close-button')
 
-function openPopup() {
-  // popup.classList.remove('popup_invisible');
-  // document.querySelector('.popup').style.display = "flex";
-  popup.classList.add('popup_visible');
+let cards = [
+  {
+    link: 'https://cdn.pixabay.com/photo/2017/02/05/17/40/saint-peters-basilica-2040718_1280.jpg',
+    text: 'Rome'
+  },
+
+  {
+    link: 'https://cdn.pixabay.com/photo/2018/07/18/20/25/channel-3547224_1280.jpg',
+    text: 'Venice'
+  },
+
+  {
+    link: 'https://cdn.pixabay.com/photo/2019/06/06/13/36/italy-4256018_1280.jpg',
+    text: 'Florence'
+  },
+
+  {
+    link: 'https://cdn.pixabay.com/photo/2018/06/26/22/19/italy-3500409_1280.jpg',
+    text: 'Lake Como'
+  },
+
+  {
+    link: 'https://cdn.pixabay.com/photo/2021/09/21/19/39/mountains-6644768_1280.jpg',
+    text: 'Positano'
+  },
+
+  {
+    link: 'https://cdn.pixabay.com/photo/2017/12/13/13/46/malcesine-3016795_1280.jpg',                                          
+    text: 'Lake Garda'
+  }
+
+]
+
+function renderCards() {
+  cards.forEach(function(card){
+
+    let div = document.createElement('div');
+    div.className = "element";
+    div.innerHTML = `<img class="element__image" alt="место" src=${card.link}> <div class="element__content"> <h2 class="element__content-title">${card.text}</h2> <button type="button" class="element__like-button"></button> </div>`;
+    document.querySelector('.elements').append(div)
+  
+  })
+}
+renderCards()
+
+
+
+
+function submitAddPopup(event) {
+  event.preventDefault()
+  let popupAddLink = document.querySelector('#link-input')
+  let popupAddText = document.querySelector('#placeAbout-input')
+
+  let newCard = {
+    link: popupAddLink.value,
+    text: popupAddText.value
+  }
+
+  cards.push(newCard)
+  closePopup()
+  removeCards()
+  renderCards()
+
+  popupAddLink.value = ""
+  popupAddText.value = ""
+}
+
+let popupFormAdd = document.querySelector('.popup__form-add')
+popupFormAdd.addEventListener('submit', submitAddPopup)
+
+
+function removeCards() {
+ 
+    let cards = document.querySelectorAll('.element')
+
+   for(let card of cards){
+     card.remove()
+   }
+}
+
+
+closeButtons.forEach(button => button.addEventListener('click', closePopup))
+
+
+function openPopupEditProfile() {
+  popupEditProfile.classList.add('popup_visible');
   popupName.value = popupTitle.textContent;
   popupJob.value = popupSubtitle.textContent;
 };
 
-popupEdit.addEventListener('click', openPopup);
+popupEdit.addEventListener('click', openPopupEditProfile);
 
 function closePopup() {
-  // document.querySelector('.popup').style.display = "none";
-  popup.classList.remove('popup_visible');
+  popupEditProfile.classList.remove('popup_visible');
+  popupAddCard.classList.remove('popup_visible');
 };
 
 
@@ -38,20 +116,30 @@ function formSubmitHandler(evt) {
   closePopup()
 }
 
-// function onLike(data) {
-//     let isActive = data.target.classList.contains('element__like-button__active');
 
-//     if (isActive) {
-//         data.target.classList.remove('element__like-button__active');
-//         data.target.classList.add('element__like-button_inactive');
-//     } else {
-//         data.target.classList.remove('element__like-button_inactive');
-//         data.target.classList.add('element__like-button__active');
-//     }
-// }
+let likes = document.querySelectorAll('.element__like-button')
+function onLike(data) {
+    let isActive = data.target.classList.contains('element__like-button__active');
+    console.log(isActive)
+    if (isActive) {
+        data.target.classList.remove('element__like-button__active');
+        data.target.classList.add('element__like-button_inactive');
+    } else {
+        data.target.classList.remove('element__like-button_inactive');
+        data.target.classList.add('element__like-button__active');
+    }
+}
 
-// likes.forEach(like => like.addEventListener('click', onLike));
+likes.forEach(like => like.addEventListener('click', onLike));
 
-popupForm.addEventListener('submit', formSubmitHandler);
-popupClose.addEventListener('click', closePopup);
-popupEdit.addEventListener('click', openPopup);
+function openPopupAddCard() {
+  popupAddCard.classList.add('popup_visible');
+
+}
+
+
+
+
+popupFormEdit.addEventListener('submit', formSubmitHandler);
+popupEdit.addEventListener('click', openPopupEditProfile);
+popupAdd.addEventListener('click', openPopupAddCard);
